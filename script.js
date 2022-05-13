07e5290be34e937aed50e5d8d3c1aeb72ed3977e
Elat <talebjorn@gmail.com>
2022-05-13 16:54:26 +0200

    Add some styling and html elements

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 const container = document.getElementById('container');
 
-for (let j = 0; j < 16; j++) {
-		const cell = document.createElement('div');
-		cell.classList.add('cell');
-		container.appendChild(cell);
-			for (let i = 0; i < 16; i++) {
-				const cell2 = document.createElement('div');
-				cell2.classList.add('cell')
-				container.appendChild(cell2);
-				}
+const options = document.getElementById('options');
+
+const reloadBtn = document.createElement('button');
+	reloadBtn.textContent = "Wipe Board";
+	reloadBtn.classList.add('reload');
+	reloadBtn.addEventListener('click', () => {
+		window.location.reload();
+	});
+
+options.appendChild(reloadBtn);
+
+let cell;
+let cell2;
+
+const gridValue = document.getElementById('range');
+
+var output = document.getElementById("value");
+output.textContent = gridValue.value;
+
+gridValue.oninput = function() {
+	output.textContent = this.value;
+  }
+
+
+for (let j = 0; j < gridValue.value; j++) {
+	let cell = document.createElement('div');
+	cell.classList.add('cell');
+	container.appendChild(cell);
+
+		for (let i = 0; i < gridValue.value; i++) {
+			let cell2 = document.createElement('div');
+			cell2.classList.add('cell');			
+			container.appendChild(cell2);	
+			
+			cell2.addEventListener('mouseover', () => {
+				cell2.classList.add('draw');
+			});
+			
 		}
+	cell.addEventListener('mouseover', () => {
+	cell.classList.add('draw');
+} );
+
+
+}
