33b9ecfc6c6859d4e310581cf810e3c61ea07f22
Elat <talebjorn@gmail.com>
2022-05-31 13:53:29 +0200

    Minor change to script and CS

--- script.js
+++ script.js
@@ -2,46 +2,56 @@ const container = document.getElementById('container');
 
 const options = document.getElementById('options');
 
-const reloadBtn = document.createElement('button');
+// const isDrawing = function () {
+// 	isDrawing = false;
+// 	isDrawing.addEventListener('mousedown', () => {
+// 		isDrawing = true;
+// 	});
+// };
+ 
+function createGrid (number) {
+	for (let i = 0; i < (number * number); i++) {
+		const cell = document.createElement('div');
+			cell.classList.add('cell');
+			container.appendChild(cell);
+			cell.addEventListener('mouseover', () => {
+				cell.classList.add('draw')
+			});
+			console.log(cell)
+
+	// const cellRemover = document.querySelectorAll('.cell');
+	// 		document.getElementById('range').addEventListener('change', () => {
+	// 			cellRemover.forEach(element => {
+	// 				container.removeChild(element);
+	// 			});
+			
+	// 		});
+
+		};
+};
+
+
+let gridSize = document.getElementById('range');
+	gridSize.addEventListener('change', (e) => {
+		container.style.setProperty('--grid-column-count', e.target.value);
+		container.style.setProperty('--grid-row-count', e.target.value); 
+		createGrid(gridSize.value);
+	});
+
+
+let output = document.getElementById('value');
+	output.textContent = gridSize.value;
+		gridSize.oninput = function() {
+		output.textContent = this.value;
+	};
+
+	const reloadBtn = document.createElement('button');
 	reloadBtn.textContent = "Wipe Board";
 	reloadBtn.classList.add('reload');
 	reloadBtn.addEventListener('click', () => {
 		window.location.reload();
 	});
 
-options.appendChild(reloadBtn);
+	options.appendChild(reloadBtn);
 
-let cell;
-let cell2;
-
-const gridValue = document.getElementById('range');
-
-var output = document.getElementById("value");
-output.textContent = gridValue.value;
-
-gridValue.oninput = function() {
-	output.textContent = this.value;
-  }
-
-
-for (let j = 0; j < gridValue.value; j++) {
-	let cell = document.createElement('div');
-	cell.classList.add('cell');
-	container.appendChild(cell);
-
-		for (let i = 0; i < gridValue.value; i++) {
-			let cell2 = document.createElement('div');
-			cell2.classList.add('cell');			
-			container.appendChild(cell2);	
-			
-			cell2.addEventListener('mouseover', () => {
-				cell2.classList.add('draw');
-			});
-			
-		}
-	cell.addEventListener('mouseover', () => {
-	cell.classList.add('draw');
-} );
-
-
-}
+	createGrid(gridSize.value);
