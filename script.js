4295633927bfeaa455e8a546c471b37423e84148
Elat <talebjorn@gmail.com>
2022-06-01 16:27:18 +0200

    Add pointerevents so drawing only occurs while mouse is pressed

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ const container = document.getElementById('container');
 
 const options = document.getElementById('options');
 
-// const isDrawing = function () {
-// 	isDrawing = false;
-// 	isDrawing.addEventListener('mousedown', () => {
-// 		isDrawing = true;
-// 	});
-// };
- 
 function createGrid (number) {
+
 	gridRemover();
+
 	for (let i = 0; i < (number * number); i++) {
 		const cell = document.createElement('div');
 			cell.classList.add('cell');
 			container.appendChild(cell);
-			cell.addEventListener('mouseover', () => {
-				cell.classList.add('draw')
-			});
-			console.log(cell)
-	
-	};
+		 
+			container.addEventListener('pointerdown', () => {
+				cell.onpointerover = () => { 
+					cell.style.background = 'pink';
+					
+				};
 
+			});	
+			container.addEventListener('pointerup', () => {
+				cell.onpointerover = () => {
+				const eraser = document.createElement('div');
+				
+				cell.style.background = 'none';
+					
+				}
+			});
+		
+
+		}
 };
 
 function gridRemover () {
@@ -35,27 +42,28 @@ function gridRemover () {
 		};
 }
 
-let gridSize = document.getElementById('range');
+	let gridSize = document.getElementById('range');
 		gridSize.addEventListener('change', (e) => {
 		container.style.setProperty('--grid-column-count', e.target.value);
 		container.style.setProperty('--grid-row-count', e.target.value); 
 		createGrid(gridSize.value);
-});
+	});
 
 
-let output = document.getElementById('value');
-	output.textContent = gridSize.value;
+	let output = document.getElementById('value');
+		output.textContent = gridSize.value;
 		gridSize.oninput = function() {
 		output.textContent = this.value;
 	};
 
 	const reloadBtn = document.createElement('button');
-	reloadBtn.textContent = "Wipe Board";
-	reloadBtn.classList.add('reload');
-	reloadBtn.addEventListener('click', () => {
-		window.location.reload();
-	});
+		reloadBtn.textContent = "Wipe Board";
+		reloadBtn.classList.add('reload');
+		reloadBtn.addEventListener('click', () => {
+			window.location.reload();
+		});
 
-	options.appendChild(reloadBtn);
+		options.appendChild(reloadBtn);
 
-	createGrid(gridSize.value);
+
+createGrid(gridSize.value);
