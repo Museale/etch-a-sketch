cdb8e5a310c8926dc8258756b5c0e95fb5980fc7
Elat <talebjorn@gmail.com>
2022-06-02 14:49:38 +0200

    Add CSS styling and minor pointerevent tweaks

--- script.js
+++ script.js
@@ -10,25 +10,35 @@ function createGrid (number) {
 		const cell = document.createElement('div');
 			cell.classList.add('cell');
 			container.appendChild(cell);
-		 
-			container.addEventListener('pointerdown', () => {
-				cell.onpointerover = () => { 
-					cell.style.background = 'pink';
-					
-				};
+	
+				container.addEventListener('pointerdown', () => {
+					cell.onpointerover = () => { 
+						cell.style.background = 'pink';
+						cell.classList.add('draw');
+					};
+					cell.onclick = () => {
+						cell.style.background = 'white';
+						cell.classList.add('draw');
+					}
+				});	
+				container.addEventListener('pointerup', () => {
+					cell.onpointerover = () => {
+					if (cell.classList.contains('draw') !== true) {
+						cell.style.background = 'none';
+					}
+					};
+				});
 
-			});	
-			container.addEventListener('pointerup', () => {
-				cell.onpointerover = () => {
-				const eraser = document.createElement('div');
-				
-				cell.style.background = 'none';
-					
+				container.addEventListener('dblclick', (e) => {
+					cell.style.cursor = 'pointer';
+					cell.onpointerover = () => {
+					if (cell.classList.contains('draw') !== false) {
+						cell.style.background = 'none';
+					}
 				}
-			});
+				})
 		
-
-		}
+	}
 };
 
 function gridRemover () {
@@ -66,4 +76,48 @@ function gridRemover () {
 		options.appendChild(reloadBtn);
 
 
+
+		// const colourCanvas = document.createElement('canvas');
+		// const colourCtx = colourCanvas .getContext('2d');
+
+		// options.appendChild(colourCanvas);
+
+
+		// let colour = 'rgba(0,0,255,1)';
+
+		// let gradientH = colourCtx .createLinearGradient(0, 0, colourCtx .canvas.width, 0);
+		// gradientH.addColorStop(0, '#fff');
+		// gradientH.addColorStop(1, colour);
+		// colourCtx .fillStyle = gradientH;
+		// colourCtx .fillRect(0, 0, colourCtx .canvas.width, colourCtx .canvas.height);
+
+		// let gradientV = colourCtx .createLinearGradient(0, 0, 0, 300);
+		// gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+		// gradientV.addColorStop(1, '#000');
+		// colourCtx .fillStyle = gradientV;
+		// colourCtx .fillRect(0, 0, colourCtx .canvas.width, colourCtx .canvas.height);
+
+
+		// colourCanvas.addEventListener('click', e => {
+		// 	let x = e.clientX;
+		// 	let y = e.clientY;
+		// 	 pixel = colourCtx.getImageData(x,y,1,1)['data'];
+		// 	 rgb = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
+		// 	console.log(rgb);
+		// 	console.log(pixel);
+		// 	container.style.background = rgb;
+		// })
+
+
+		// colourCanvas.classList.add('colourCanvas');
+
+
+		
+
+
+
+
+
+
 createGrid(gridSize.value);
+
